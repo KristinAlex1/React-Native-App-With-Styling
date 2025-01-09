02b88bab099fef47ab8c703b9a118e6bcd66ba0d
KristinAlex1 <kristinalex2001@gmail.com>
2025-01-09 17:36:22 -0400

    Completed and styled ContactList

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -8,7 +8,7 @@ export default function ContactList() {
             name: 'John Doe',
             phone: '123-456-7890',
             email: 'John@Doe.com',
-            imageUrl: '...images/John.jpg'
+            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjUuYcnZ-xqlGZiDZvuUy_iLx3Nj6LSaZSzQ&s'
         
         },
         {
@@ -16,14 +16,14 @@ export default function ContactList() {
             name: 'Jane Doe',
             phone: '123-456-7891',
             email: 'Jane@Doe.com,',
-            imageUrl: '...images/Jane.jpg'
+            imageUrl: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
         },
         {   
             uid: 'u3',
             name: 'Joe Doe',
             phone: '123-456-7892',
             email: 'Joe@Doe.com,',
-            imageUrl: '...images/Joe.jpg'
+            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQi2Mm5P8j09P4hPKa1B-t9eIOHzHmR7IBkw&s'
 
         }
     ]
@@ -36,9 +36,13 @@ export default function ContactList() {
                 <Image source = {{
                     uri: imageUrl,
                 }} style = {styles.userImage}/>
+                <View>
                 <Text style = {styles.userName}>{name}</Text>
                 <Text style = {styles.userPhone}>{phone}</Text>
                 <Text style = {styles.userEmail}>{email}</Text>
+
+                </View>
+                
 
             </View>
         ))}
@@ -50,28 +54,53 @@ export default function ContactList() {
 
 const styles = StyleSheet.create({
     headingText:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 8,
 
     },
     container:{
+        paddingHorizontal:20,
+        marginBottom: 20,
 
     },
     userCard:{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 120,
+        width: 350,
+        marginLeft: 10,
+        marginRight: 20,
+        borderRadius: 20,
+        marginBottom: 20,
+        backgroundColor: '#FCDFA6',
+        marginTop: 10,
+
 
     },
     userImage:{
-        height:200,
-        width:200,
-        borderRadius: 50,
+        marginVertical: 10,
+        height:120,
+        width:120,
+        borderRadius: 60,
+        marginRight: 20,
         resizeMode: 'contain',
+        marginLeft: 10,
 
     },
     userName:{
+        fontWeight: 'bold',
+        fontSize: 20,
 
     },
     userPhone:{
+        fontSize: 15,
 
     },
     userEmail:{
+        fontSize: 15,
 
     },
 })
